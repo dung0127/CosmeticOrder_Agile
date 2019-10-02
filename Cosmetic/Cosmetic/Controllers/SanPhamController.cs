e37fb8894e69275d747351305b75ec73ad963112
Châu Thái Bảo <44626067+thaibao236@users.noreply.github.com>
2019-10-02 22:32:34 +0700

    FDO-14: Show list of product
    FDO-14: Show list of product

---
+++ Cosmetic/Cosmetic/Controllers/SanPhamController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReflectionIT.Mvc.Paging;
+using WebOnline.Models;
+
+namespace WebOnline.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        private readonly MyPhamContext db;
+        public SanPhamController(MyPhamContext context)
+        {
+            db = context;
+        }
+
+
+        [Route("san-pham/{loai}")]
+        public async Task<IActionResult> Index(string loai, int page = 1)
+        {
+
+            var qry = db.SanPham.AsNoTracking().OrderBy(p => p.MaSp);
+            var model = await PagingList.CreateAsync(qry, 9, page);
+            if (loai != null)
+            {
+                ViewBag.Loai = db.Loai.SingleOrDefault(p => p.TenLoaiSeoUrl == loai);
+                Loai qery1 = db.Loai.SingleOrDefault(p => p.TenLoaiSeoUrl == loai);
+                var qery = db.SanPham.Where(p => p.MaLoai == qery1.MaLoai).AsNoTracking().OrderBy(p => p.MaSp);
+
+                var md = await PagingList.CreateAsync(qery, 9, page);
+                return View(md);
+            }
+
+            return View(model);
+        }
+    }
+}
