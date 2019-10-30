e0fbe980182b50b069484ff3030fd060afc236ac
ViHa0509 <43596575+ViHa0509@users.noreply.github.com>
2019-10-30 22:54:37 +0700

    CO-6: Create Admin page layout

---
+++ Cosmetic/Cosmetic/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cosmetic.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Route("[controller]/[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
