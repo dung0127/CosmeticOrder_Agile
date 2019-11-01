873c475d81323be236fc06f034179bebd4698453
ViHa0509 <43596575+ViHa0509@users.noreply.github.com>
2019-11-01 21:00:46 +0700

    CO-6: Child Issue - Layout login form

--- Cosmetic/Cosmetic/Controllers/AdminController.cs
+++ Cosmetic/Cosmetic/Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cosmetic.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cosmetic.Controllers
@@ -10,6 +11,23 @@ namespace Cosmetic.Controllers
     {
         [Route("[controller]/[action]")]
         public IActionResult Index()
+        {
+            if (HttpContext.Session.Get<NhanVien>("MaNv") == null)
+            {
+                return Redirect("/Admin/Login");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        private readonly MyPhamContext db;
+        public AdminController(MyPhamContext context)
+        {
+            db = context;
+        }
+        [Route("[controller]/[action]")]
+        public IActionResult Login()
         {
             return View();
         }
